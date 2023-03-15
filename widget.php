7bd15d80c1f05ffcd367b8f46bbeba7e99ff0311
Steven Sullivan <17299026+SS88UK@users.noreply.github.com>
2023-03-15 17:32:04 -0400

    v1.4

--- widget.php
+++ widget.php
@@ -1,15 +1,15 @@
 <?php
 /*
-Plugin Name: VestaCP Dashboard Widget
-Plugin URI: https://ss88.uk/
-Description: Adds a widget to the Dashboard showing your VestaCP accounts details. Requires an API key from VestaCP.
-Version: 1.0
-Author: Steven Sullivan Ltd
-Author URI: https://blog.ss88.uk/
+Plugin Name: VestaCP/myVesta Dashboard Widget
+Plugin URI: https://blog.ss88.us/vestacp-dashboard-widget-for-wordpress
+Description: Adds a widget to the Dashboard showing your VestaCP/myVesta accounts details. Requires an API key from VestaCP.
+Version: 1.4
+Author: Steven Sullivan
+Author URI: https://blog.ss88.us
 License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
-
+define('SS88VESTA_VERSION', 1.4);
 // Add the Widget to WordPress
 function ss88_add_vestacp_widget() { wp_add_dashboard_widget('ss88_vestacp_widget', 'VestaCP', 'ss88_show_vestacp_widget');	}
 
@@ -17,14 +17,15 @@ function ss88_add_vestacp_widget() { wp_add_dashboard_widget('ss88_vestacp_widge
 function ss88_show_vestacp_widget() {
 	
 	// Ouput the style and scripts
-	wp_enqueue_style('ss88_vestacp_widget_css', plugins_url('css.css', __FILE__));
-	wp_enqueue_script('ss88_vestacp_widget_js', plugins_url('js.js', __FILE__));
+	wp_enqueue_style('ss88_vestacp_widget_css', plugins_url('css.css', __FILE__), false, SS88VESTA_VERSION);
+	wp_enqueue_script('ss88_vestacp_widget_js', plugins_url('js.js', __FILE__), false, SS88VESTA_VERSION);
 	
 	// Get Vesta Details
 	$Vesta = array();
 	$Vesta['URL'] = get_option('ss88_vestacp_widget_vesta_url');
 	$Vesta['Username'] = get_option('ss88_vestacp_widget_vesta_username');
 	$Vesta['Hash'] = get_option('ss88_vestacp_widget_vesta_hash');
+	$Vesta['VerifySSL'] = get_option('ss88_vestacp_widget_vesta_verifyssl');
 	
 	if($Vesta['URL'] != '' && $Vesta['Username'] != '' && $Vesta['Hash'] != '')
 	{
@@ -40,7 +41,7 @@ function ss88_show_vestacp_widget() {
 	}
 	
 	// Show Steven Sullivan Ltd Copy
-	echo '<ul class="ss88_vw_keys k2"><li class="red"><a href="https://paypal.me/SS88/3" target="_blank">Beers Please!</a></li><li class="violet"><a href="https://blog.ss88.uk/" target="_blank">Steven Sullivan Ltd</a></li></ul><div class="ss88_spinner"><div class="double-bounce1"></div><div class="double-bounce2"></div></div>';
+	echo '<ul class="ss88_vw_keys k2"><li class="red"><a href="https://paypal.me/SS88/3" target="_blank">Beers Please!</a></li><li class="violet" style="background-color:#ff0079;"><a href="https://blog.ss88.us/" target="_blank">SS88 LLC</a></li></ul><div class="ss88_spinner"><div class="double-bounce1"></div><div class="double-bounce2"></div></div>';
 }
 
 
@@ -53,8 +54,8 @@ function ss88_vestacp_widget_api($Vesta)
 		'arg1' => $Vesta['Username'],
 		'arg2' => 'json'
 	);
-
-	$answer = wp_remote_post($Vesta['URL'] . '/api/', array('body'=>$postvars));
+	
+	$answer = wp_remote_post($Vesta['URL'] . '/api/', array('sslverify'=>$Vesta['VerifySSL'], 'body'=>$postvars));
 	
 	// Return Error
 	if (is_wp_error($answer))
@@ -83,16 +84,18 @@ function ss88_vestacp_widget_ajax()
 	$Vesta['URL'] = sanitize_text_field($_POST['v_url']);
 	$Vesta['Username'] = sanitize_text_field($_POST['v_username']);
 	$Vesta['Hash'] = sanitize_text_field($_POST['v_hash']);
-	
-	// Set Options
-	update_option('ss88_vestacp_widget_vesta_url', $Vesta['URL']);
-	update_option('ss88_vestacp_widget_vesta_username', $Vesta['Username']);
-	update_option('ss88_vestacp_widget_vesta_hash', $Vesta['Hash']);
+	$Vesta['VerifySSL'] = sanitize_text_field($_POST['v_verifyssl']);
 	
 	$Data = ss88_vestacp_widget_api($Vesta);
 	
 	if(is_array($Data))
 	{
+		// Set Options
+		update_option('ss88_vestacp_widget_vesta_url', $Vesta['URL']);
+		update_option('ss88_vestacp_widget_vesta_username', $Vesta['Username']);
+		update_option('ss88_vestacp_widget_vesta_hash', $Vesta['Hash']);
+		update_option('ss88_vestacp_widget_vesta_verifyssl', $Vesta['VerifySSL']);
+	
 		echo ss88_vestacp_widget_showbars($Data, $Vesta);
 	}
 	else
@@ -110,11 +113,18 @@ add_action('wp_ajax_ss88_vestacp_widget_ajax', 'ss88_vestacp_widget_ajax');
 
 function ss88_vestacp_widget_showbars($Data, $Vesta)
 {
+	
+	if($Data=='error' || empty($Data)) return 'There was an error communicating with the VestaCP server.';
 	$Data = $Data[$Vesta['Username']];
 	
-	$DiskPercent = (($Data['U_DISK'] / $Data['DISK_QUOTA']) * 100);
-	$DiskPercent = ($DiskPercent>100) ? 100 : $DiskPercent;
-	$DiskPercent = ($Data['DISK_QUOTA']=='unlimited') ? 'U' : $DiskPercent;
+	if($Data['U_DISK']!=0 && $Data['DISK_QUOTA']!=0) {
+		
+		$DiskPercent = (($Data['U_DISK'] / $Data['DISK_QUOTA']) * 100);
+		$DiskPercent = ($DiskPercent>100) ? 100 : $DiskPercent;
+		
+	}
+	
+	$DiskPercent = ($Data['DISK_QUOTA']=='unlimited') ? 'U' : 0;
 	
 	$BWPercent = (($Data['U_BANDWIDTH'] / $Data['BANDWIDTH']) * 100);
 	$BWPercent = ($BWPercent>100) ? 100 : $BWPercent;
@@ -160,6 +170,8 @@ function ss88_vestacp_widget_showform()
 	<input type="text" name="ss88_vestacp_widget_vesta_username" autocomplete="off" placeholder="Username of account to fetch" required />
 	<input type="text" name="ss88_vestacp_widget_vesta_hash" autocomplete="off" placeholder="API key" required />
 	<p style="font-size:11px;">To generate an API key, first login to the server as root and run the command <b>/usr/local/vesta/bin/v-generate-api-key</b></p>
+    <input type="checkbox" name="ss88_vestacp_widget_vesta_verifyssl" value="Y" style="width:auto;display:inline-block;margin-bottom: 0;" checked /> Verify SSL?
+    <p style="font-size:11px;">If you un-check this option, then VestaCP\'s SSL certificate will not be verified. You should only un-check this option if your VestaCP control panel uses a self-signed or invalid certificate that is controlled by you.<b>Do so at your own risk</b>.</p>
 	<input type="submit" value="Save Details" class="btn" />
 	<input type="hidden" name="ss88_vestacp_widget_nonce" value="'.$nonce.'" />
 	</form></div>
@@ -195,7 +207,7 @@ function ss88_vestacp_widget_showform()
 
 function ss88_add_vestacp_widget_formatBytes($mb) { 
 	
-	$r = $mb / 1024;
+	$r = intval($mb) / 1024;
 
     return round($r, 2) . 'GB'; 
 } 
@@ -205,6 +217,7 @@ function ss88_add_vestacp_widget_remove()
 	delete_option('ss88_vestacp_widget_vesta_url');
 	delete_option('ss88_vestacp_widget_vesta_username');
 	delete_option('ss88_vestacp_widget_vesta_hash');
+	delete_option('ss88_vestacp_widget_vesta_verifyssl');
 }
 
 register_uninstall_hook( __FILE__, 'ss88_add_vestacp_widget_remove' );
